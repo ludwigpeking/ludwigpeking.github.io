e96b838d794a416002c82741eeda696fa1c8467c
ludwigpeking <ludwigpeking@gmail.com>
2023-06-14 15:54:53 +0800

    test

---
+++ sketch.js
@@ -0,0 +1,10 @@
+const { text } = require("d3");
+
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    text("This Is Ludwigpeking.github.io", 20, 30)
+  }
