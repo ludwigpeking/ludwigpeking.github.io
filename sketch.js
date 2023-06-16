b38d2b8dd9e913da3e11952a14125f608ef1ea7d
Qian Li (ludwig.peking) <ludwig.peking@gmail.com>
2023-06-16 16:18:19 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,5 +4,8 @@ function setup() {
   
   function draw() {
     background(220);
+    fill(0,255,255);
+    noStroke();
+    rect(width/2, height/2, 20, 20);
     text("This Is Ludwigpeking.github.io", 20, 30)
   }
