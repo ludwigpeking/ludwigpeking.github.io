0037e75886144114452e77eb3f61736aafafba11
ludwigpeking <ludwigpeking@gmail.com>
2023-08-10 13:58:40 +0800

    svg changed

--- voronoiBack.js
+++ voronoiBack.js
@@ -1,20 +1,32 @@
 var points = [];
 var colors = [];
+let voronoi, svg, pathGroup, circleGroup;
 // Make 50 random points
-for (var i = 0; i < 50; i++) {
-    points.push([Math.random() * window.innerWidth, Math.random() * window.innerHeight]);
-    colors.push(randomColor());
+restart();
+
+function restart() {
+    for (let i = 0; i < 50; i++) {
+        points.push([Math.random() * 2540, Math.random() * 1600]);
+        colors.push(randomColor());
+    }
+
+    svg = d3.select("#voronoi-container").append("svg")
+        .attr("width", 2540)
+        .attr("height", 1600);
+
+    voronoi = d3.voronoi().extent([[-1, -1], [2540 + 1, 1600 + 1]]);
+    pathGroup = svg.append("g");
+    circleGroup = svg.append("g"); // Added group for points
 }
 
-var svg = d3.select("#voronoi-container").append("svg")
-    .attr("width", window.innerWidth)
-    .attr("height", window.innerHeight);
-
-var voronoi = d3.voronoi().extent([[-1, -1], [window.innerWidth + 1, window.innerHeight + 1]]);
-var pathGroup = svg.append("g");
-var circleGroup = svg.append("g"); // Added group for points
-
 function update() {
+    //if points are more than 1000, empty points array and colors array using restart function
+    if (points.length > 1000) {
+        points = [];
+        colors = [];
+        svg.remove();
+        restart();
+    }
     var diagram = voronoi(points);
     var polygons = diagram.polygons();
 
@@ -25,9 +37,9 @@ function update() {
         .merge(paths) // Merge with existing paths
         .attr("d", function(d) { return d ? "M" + d.join("L") + "Z" : null; })
         .attr("fill", function(d, i) { return colors[i]; })
-        .attr("fill-opacity", 0.1)
-        // .attr("stroke", "black")
-        // .attr("stroke-width", 2);
+        .attr("fill-opacity", 1)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1);
 
     paths.exit().remove();
 
@@ -45,7 +57,7 @@ function update() {
 }
 
 function randomColor() {
-    let randomColor = Math.floor(Math.random()*100 + 16).toString(16);
+    let randomColor = Math.floor(Math.random()*30 + 16).toString(16);
     var color  = '#' + randomColor + randomColor + randomColor;
     return color;
 }
