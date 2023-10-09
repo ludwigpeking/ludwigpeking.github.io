f144a412aadf825a75b5d92e0ce1f4ae0251e69a
ludwigpeking <ludwigpeking@gmail.com>
2023-10-09 15:18:21 +0800

    adding project info

--- script.js
+++ script.js
@@ -4,34 +4,33 @@ const imageContainer = document.getElementById('image-container');
 
 // Array of image file paths (modify as needed)
 const imageFiles = [
-  './img/Places_resized/11_Tsingtao_01.jpg',
-  './img/Places_resized/11_Tsingtao_02.jpg',
-      './img/Places_resized/11_Tsingtao_03.jpg',
-    './img/Places_resized/11_Tsingtao_10.jpg',
-    './img/Places_resized/11_Tsingtao_20.jpg',
-    './img/Places_resized/11_Tsingtao_21.jpg',
-    './img/Places_resized/11_Tsingtao_22.jpg',
-    './img/Places_resized/12_Jilin_01.jpg',
-    './img/Places_resized/12_Jilin_02.jpg',
-    './img/Places_resized/12_Jilin_03.jpg',
-    './img/Places_resized/12_Tianjin_01.jpg',
-    './img/Places_resized/12_Tianjin_10.jpg',
-    './img/Places_resized/12_Tsingtao_01.jpg',
+  './img/2011-2019/Places_resized/11_Tsingtao_01.jpg',
+  './img/2011-2019/Places_resized/11_Tsingtao_02.jpg',
+    './img/2011-2019/Places_resized/11_Tsingtao_03.jpg',
+    './img/2011-2019/Places_resized/11_Tsingtao_10.jpg',
+    './img/2011-2019/Places_resized/11_Tsingtao_20.jpg',
+    './img/2011-2019/Places_resized/11_Tsingtao_21.jpg',
+    './img/2011-2019/Places_resized/11_Tsingtao_22.jpg',
+    './img/2011-2019/Places_resized/12_Jilin_01.jpg',
+    './img/2011-2019/Places_resized/12_Jilin_02.jpg',
+    './img/2011-2019/Places_resized/12_Jilin_03.jpg',
+    './img/2011-2019/Places_resized/12_Tianjin_01.jpg',
+    './img/2011-2019/Places_resized/12_Tianjin_10.jpg',
+    './img/2011-2019/Places_resized/12_Tsingtao_01.jpg',
   // ... more file paths ...
 ];
 
 imageFiles.forEach(imageSrc => {
   const previewDiv = document.createElement('div');
   previewDiv.className = 'image-preview';
-
   const img = document.createElement('img');
   img.src = imageSrc;
-
   previewDiv.appendChild(img);
   imageContainer.appendChild(previewDiv);
 });
 
 function loadProject(file) {
+  console.log('Loading project:', file);
   const contentContainer = document.getElementById('dynamic-content');
 
   fetch(file)
