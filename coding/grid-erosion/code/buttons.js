146c116153b63a084c0bcb34c3b1c1eada12f352
ludwigpeking <ludwigpeking@gmail.com>
2023-11-20 12:31:37 +0800

    updated grid-erosion auto proceed

--- coding/grid-erosion/code/buttons.js
+++ coding/grid-erosion/code/buttons.js
@@ -1,241 +1,347 @@
-
 let buttonList = [];
-let inputElements = []; 
-let labelElements = []; 
+let inputElements = [];
+let labelElements = [];
 let sceneSelect;
-let _sceneTitleElement ;
+let _sceneTitleElement;
 let showProsperity = false;
 let isTrafficShown = false;
 
-
-
 const params = [
-    {name: 'streetCount', type: 'int'},
-    {name: 'avenueCount', type: 'int'},
-    {name: 'streetWidth', type: 'int'},
-    {name: 'blockParcelCount', type: 'int'},
-    {name: 'parcelWidth', type: 'int'},
-    {name: 'parcelDepth', type: 'int'},
-    {name: 'res', type: 'int'},
-
+  { name: "streetCount", type: "int" },
+  { name: "avenueCount", type: "int" },
+  { name: "streetWidth", type: "int" },
+  { name: "blockParcelCount", type: "int" },
+  { name: "parcelWidth", type: "int" },
+  { name: "parcelDepth", type: "int" },
+  { name: "res", type: "int" },
 ];
 
 function getSceneTitleElement() {
-    if (!_sceneTitleElement) {
-        _sceneTitleElement = select('#scene-title');
-    }
-    return _sceneTitleElement;
+  if (!_sceneTitleElement) {
+    _sceneTitleElement = select("#scene-title");
+  }
+  return _sceneTitleElement;
 }
 
 function resetParameters() {
-    currentParams = scenes.newYork;
-    console.log('Parameters reset to default');
+  currentParams = scenes.newYork;
+  console.log("Parameters reset to default");
+  buttons();
 }
 
 function inputs() {
-    if (sceneSelect) {
-        sceneSelect.remove();
-    }
-    inputElements.forEach(input => input.remove());
-    labelElements.forEach(label => label.remove());
-    inputElements = [];
-    labelElements = [];
+  if (sceneSelect) {
+    sceneSelect.remove();
+  }
+  inputElements.forEach((input) => input.remove());
+  labelElements.forEach((label) => label.remove());
+  inputElements = [];
+  labelElements = [];
 
-    sceneSelect = createSelect();
-    sceneSelect.position(10, height + 90).style('padding', "5px"); // Adjust position as needed
-    for (const scene in scenes) {
-        sceneSelect.option(scene);
+  sceneSelect = createSelect();
+  sceneSelect.position(10, height + 90).style("padding", "5px"); // Adjust position as needed
+  for (const scene in scenes) {
+    sceneSelect.option(scene);
+  }
+  sceneSelect.changed(() => {
+    selectedScene = sceneSelect.value();
+    changeScene(selectedScene);
+  });
+  sceneSelect.value(selectedScene);
+
+  inputElements.push(sceneSelect); // To ensure it gets removed and updated like other inputs
+  const sceneTitle = getSceneTitleElement();
+  if (sceneTitle) {
+    sceneTitle
+      .html(capitalizeFirstLetter(selectedScene) + " Grid")
+      .style("top", "10px");
+  }
+
+  inputElements = params.map((param, index) => {
+    const label = createElement("label", capitalizeFirstLetter(param.name))
+      .position(index * 100 + 10, height + 120)
+      .style("font-size", "12px");
+
+    labelElements.push(label); // Store label elements to update them later
+
+    const input = createInput("")
+      .value(currentParams[param.name])
+      .position(index * 100 + 10, height + 140)
+      .size(87, 20);
+
+    if (param.type === "int") {
+      input.attribute("type", "number").attribute("step", "1");
     }
-    sceneSelect.changed(() => {
-        selectedScene = sceneSelect.value();
-        changeScene(selectedScene);
+
+    input.changed(() => {
+      updateParameter(param.name, input.value(), param.type);
     });
-    sceneSelect.value(selectedScene);
-    
-    inputElements.push(sceneSelect); // To ensure it gets removed and updated like other inputs
-    const sceneTitle = getSceneTitleElement();
-    if (sceneTitle) {
-        sceneTitle.html(capitalizeFirstLetter (selectedScene) + " Grid").style( 'top', '10px');
-    }
 
-    inputElements = params.map((param, index) => {
-        const label = createElement('label', capitalizeFirstLetter(param.name) )
-            .position(index * 100+10, height + 120)
-            .style('font-size', '12px');
-        
-        labelElements.push(label);  // Store label elements to update them later
-
-        const input = createInput('')
-            .value(currentParams[param.name])
-            .position(index * 100+10, height + 140)
-            .size(87, 20);
-
-        if (param.type === 'int') {
-            input.attribute('type', 'number').attribute('step', '1');
-        }
-
-        input.changed(() => {
-            updateParameter(param.name, input.value(), param.type);
-        });
-
-        return input;
-    });
+    return input;
+  });
 }
 
-
 function getDefaultValue(paramName) {
-    return currentParams[paramName] || '';
+  return currentParams[paramName] || "";
 }
 
 function createNumberInput(paramName) {
-    const input = createInput('');
-    input.attribute('type', 'number');
-    input.attribute('step', '1'); // Increment by 1
-    input.value(window[paramName]); // Set the default value
-    return input;
+  const input = createInput("");
+  input.attribute("type", "number");
+  input.attribute("step", "1"); // Increment by 1
+  input.value(window[paramName]); // Set the default value
+  return input;
 }
 
 function updateParameter(paramName, value, type) {
-    if (type === 'int') {
-        value = parseInt(value);
-    }
-    currentParams[paramName] = value;
-    needUpdate = true;
-    
-    const labelIndex = params.findIndex(param => param.name === paramName);
-    if (labelIndex !== -1) {
-        labelElements[labelIndex].html(capitalizeFirstLetter(paramName) + ': ' + value);
-    }
-    ({  streetCount, avenueCount, streetWidth, blockParcelCount, parcelWidth, parcelDepth, res, parkEdges, parkContinue, needUpdate, squareEdges, broadway} = currentParams);
+  if (type === "int") {
+    value = parseInt(value);
+  }
+  currentParams[paramName] = value;
+  needUpdate = true;
+
+  const labelIndex = params.findIndex((param) => param.name === paramName);
+  if (labelIndex !== -1) {
+    labelElements[labelIndex].html(
+      capitalizeFirstLetter(paramName) + ": " + value
+    );
+  }
+  ({
+    streetCount,
+    avenueCount,
+    streetWidth,
+    blockParcelCount,
+    parcelWidth,
+    parcelDepth,
+    res,
+    parkEdges,
+    parkContinue,
+    needUpdate,
+    squareEdges,
+    broadway,
+  } = currentParams);
+  buttons();
 }
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function buttons() {
-    const buttonHeight = 40;
-    const buttonLineHeight = 45;
-    const buttonWidth = 150;
-    const buttonData = [
-        { label: 'Traffic!', gridPosition: {i: 0, j: 0}, mousePressed: generateRandomTraffic10 },
-        { label: 'More Traffic!<br>(Wait a Few Secs)', gridPosition: {i: 1, j: 0}, mousePressed: generateRandomTraffic100 },
-        { label: 'Lot More Traffic! <br>(Wait a Few Secs)', gridPosition: {i: 2, j: 0}, mousePressed: generateRandomTraffic300 },
-        { label: (showProsperity ? 'Hide' : 'Show') + ' Parcel Prosperity', gridPosition: {i: 3, j: 0}, mousePressed: redrawTheParcelsProsperity },
+  const buttonHeight = 40;
+  const buttonLineHeight = 45;
+  const buttonWidth = 150;
+  const progressBarWidth = 50; // Width of the progress bar
+  const progressBarHeight = 40;
+  for (let btn of buttonList) {
+    btn.remove();
+  }
+  buttonList = [];
+  const buttonData = [
+    {
+      label: "Auto",
+      gridPosition: { i: 0, j: 0 },
+      mousePressed: () => autoGeneration(),
+    },
+    {
+      label: "Traffic!",
+      gridPosition: { i: 0, j: 1 },
+      mousePressed: () => generateRandomTraffic10(),
+    },
+    {
+      label: "More Traffic!<br>(Wait a Few Secs)",
+      gridPosition: { i: 1, j: 1 },
+      mousePressed: () => generateRandomTraffic100(),
+    },
+    {
+      label: "Lot More Traffic! <br>(Wait a Few Secs)",
+      gridPosition: { i: 2, j: 1 },
+      mousePressed: () => generateRandomTraffic300(),
+    },
+    {
+      label: (showProsperity ? "Hide" : "Show") + " Parcel Prosperity",
+      gridPosition: { i: 3, j: 1 },
+      mousePressed: () => redrawTheParcelsProsperity(),
+    },
 
+    {
+      label: "Destroy 20% Random Parcels",
+      gridPosition: { i: 0, j: 2 },
+      mousePressed: () => destroyRandomParcels(),
+    },
+    {
+      label: "Destroy 10% Low Prosperity Parcels",
+      gridPosition: { i: 1, j: 2 },
+      mousePressed: () => destoryParcelsLowProsper(),
+    },
+    {
+      label: "Reduce Prosperity of All Parcels by 50%",
+      gridPosition: { i: 2, j: 2 },
+      mousePressed: () => destoryParcelsHalfProsper(),
+    },
+    {
+      label: "Traffic Decline 50%",
+      gridPosition: { i: 3, j: 2 },
+      mousePressed: () => trafficDecline(),
+    },
 
-        { label: 'Destroy 20% Random Parcels', gridPosition: {i: 0, j: 1}, mousePressed: destroyRandomParcels },
-        { label: 'Destroy 10% Low Prosperity Parcels', gridPosition: {i: 1, j: 1}, mousePressed: destoryParcelsLowProsper },
-        { label: 'Reduce Prosperity of All Parcels by 50%', gridPosition: {i: 2, j: 1}, mousePressed: destoryParcelsHalfProsper },
-        { label: 'Traffic Decline 50%', gridPosition: {i: 3, j: 1}, mousePressed: trafficDecline },
+    {
+      label: "Make Informal Parcels",
+      gridPosition: { i: 0, j: 3 },
+      mousePressed: () => spawnOneParcel(),
+    },
+    {
+      label: "Claim Tiles",
+      gridPosition: { i: 1, j: 3 },
+      mousePressed: () => parcelClaimOneTile(),
+    },
 
-        { label: 'Make Informal Parcels', gridPosition: {i: 0, j: 2}, mousePressed: spawnOneParcel },
-        { label: 'Claim Tiles', gridPosition: {i: 1, j: 2}, mousePressed: parcelClaimOneTile },
+    {
+      label: "Traffic Clear",
+      gridPosition: { i: 0, j: 4 },
+      mousePressed: () => trafficClear(),
+    },
+    {
+      label: "Reset to Default Parameters",
+      gridPosition: { i: 1, j: 4 },
+      mousePressed: () => resetParameters(),
+    },
+    {
+      label: (isTrafficShown ? "Hide" : "Show") + " Traffic",
+      gridPosition: { i: 2, j: 4 },
+      mousePressed: () => showTraffic(),
+    },
+  ];
 
-        { label: 'Traffic Clear', gridPosition: {i: 0, j: 3}, mousePressed: trafficClear },
-        { label: 'Reset to Default Parameters', gridPosition: {i: 1, j: 3}, mousePressed: resetParameters },
-        { label: (isTrafficShown ? 'Hide' : 'Show') + ' Traffic', gridPosition: {i: 2, j: 3}, mousePressed: showTraffic }
+  buttonData.forEach((button) => {
+    const x = button.gridPosition.i * buttonWidth + 10;
+    const y = height + 170 + button.gridPosition.j * buttonLineHeight;
 
-    ];
-    
-
-    buttonData.forEach((button) => {
-        const x = button.gridPosition.i * buttonWidth +10;
-        const y = height + 170 + button.gridPosition.j * buttonLineHeight;
-
-        const btn = createButton(button.label);
-        btn.position(x, y);
-        btn.size(buttonWidth - 5, buttonHeight);
-        btn.mousePressed(button.mousePressed);
-        buttonList.push(btn);
-    });
+    const btn = createButton(button.label);
+    btn.position(x, y);
+    btn.size(buttonWidth - 5, buttonHeight);
+    btn.mousePressed(button.mousePressed);
+    if (button.label === "Auto") {
+      btn.style("font-weight", "bold"); // Make text bold
+      btn.style("background-color", "#eac"); // Set background color to pink
+    }
+    buttonList.push(btn);
+  });
+  const existingProgressBar = document.getElementById("progressBar");
+  if (existingProgressBar) {
+    existingProgressBar.remove();
+  }
+  const progressBar = createDiv("").position(160, height + 170); // Adjust position as needed
+  progressBar.size(50, 40); // Set size of the progress bar
+  progressBar.id("progressBar");
 }
 
-function generateRandomTraffic() {
+function generateRandomTraffic(times = 1) {
+  for (let i = 0; i < times; i++) {
     let start = weightedRandom(parcels, "prosperity").accessPoint;
     let end = weightedRandom(parcels, "prosperity").accessPoint;
     // fill(255, 0, 0);
     // rect(start.i * res, start.j * res, res, res);
     // rect(end.i * res, end.j * res, res, res);
     pathfinding(start, end);
+  }
 }
 
-function generateRandomTraffic10(){
-    for(let i = 0 ; i<10; i++)generateRandomTraffic()
+function generateRandomTraffic10() {
+  generateRandomTraffic(10);
 }
 
 function generateRandomTraffic300() {
-    for(let i = 0 ; i<300; i++)generateRandomTraffic()
+  generateRandomTraffic(300);
 }
 
 function generateRandomTraffic100() {
-    for(let i = 0 ; i<100; i++)generateRandomTraffic()
+  generateRandomTraffic(100);
 }
-function redrawTheParcelsProsperity(){
-    const btn = buttonList.find(b => b.html() === "Show Parcel Prosperity" || b.html() === "Hide Parcel Prosperity");
-    
-    if (!showProsperity){
-        console.log('redrawTheParcelsProsperity');
-        for(const parcel of parcels){
-            for(const tile of parcel.tiles){
-                fill(255-10* sqrt(parcel.prosperity), 255-10* sqrt(parcel.prosperity), 255);
-                noStroke();
-                rect(tile.i * res, tile.j * res, res, res);
-            }
-        } 
-        showProsperity = true;
+function redrawTheParcelsProsperity() {
+  const btn = buttonList.find(
+    (b) =>
+      b.html() === "Show Parcel Prosperity" ||
+      b.html() === "Hide Parcel Prosperity"
+  );
 
-        // change button label to "Hide Parcel Prosperity"
-        if (btn) btn.html("Hide Parcel Prosperity");
-    } else {
-        for (let tile of tiles){
-            if (parcels.includes(tile.owner))tile.show();
-        }
-        showProsperity = false;
-
-        // change button label to "Show Parcel Prosperity"
-        if (btn) btn.html("Show Parcel Prosperity");
+  if (!showProsperity) {
+    console.log("redrawTheParcelsProsperity");
+    for (const parcel of parcels) {
+      for (const tile of parcel.tiles) {
+        fill(
+          255 - 10 * sqrt(parcel.prosperity),
+          255 - 10 * sqrt(parcel.prosperity),
+          255
+        );
+        noStroke();
+        rect(tile.i * res, tile.j * res, res, res);
+      }
     }
+    showProsperity = true;
+
+    // change button label to "Hide Parcel Prosperity"
+    if (btn) btn.html("Hide Parcel Prosperity");
+  } else {
+    for (let tile of tiles) {
+      if (parcels.includes(tile.owner)) tile.show();
+    }
+    showProsperity = false;
+
+    // change button label to "Show Parcel Prosperity"
+    if (btn) btn.html("Show Parcel Prosperity");
+  }
 }
 
 //remove Buttons and inputs when updating parameters
-function removeButtonsAndInputsIfThereAre(){
-    for (let btn of buttonList) {
-        btn.remove();
-    }
-    buttonList = [];  // Clear the button list
+function removeButtonsAndInputsIfThereAre() {
+  for (let btn of buttonList) {
+    btn.remove();
+  }
+  buttonList = []; // Clear the button list
 
-    for (let input of inputElements) {
-        input.remove();
-    }
-    inputElements = [];  // Clear the input list
+  for (let input of inputElements) {
+    input.remove();
+  }
+  inputElements = []; // Clear the input list
 }
 
 function setSceneInDropdown(sceneName) {
-    sceneSelect.selected(sceneName); // using selected() function if it's p5.js
+  sceneSelect.selected(sceneName); // using selected() function if it's p5.js
 }
 
 function changeScene(sceneName) {
-    const scene = scenes[sceneName];
-    const sceneTitle = getSceneTitleElement();
-    if (sceneTitle) {
-        sceneTitle.html(capitalizeFirstLetter(`${sceneName}`));
-    }
-    if (scene) {
-        currentParams = scene;
-        updateGlobalParametersFromCurrent();
-        needUpdate = true;
+  const scene = scenes[sceneName];
+  const sceneTitle = getSceneTitleElement();
+  if (sceneTitle) {
+    sceneTitle.html(capitalizeFirstLetter(`${sceneName}`));
+  }
+  if (scene) {
+    currentParams = scene;
+    updateGlobalParametersFromCurrent();
+    needUpdate = true;
 
-        setSceneInDropdown(sceneName); // Update the dropdown value
-
-        console.log(`Switched to ${sceneName} scene`);
-    } else {
-        console.log(`Scene ${sceneName} not found`);
-    }
+    setSceneInDropdown(sceneName); // Update the dropdown value
+    buttons();
+    console.log(`Switched to ${sceneName} scene`);
+  } else {
+    console.log(`Scene ${sceneName} not found`);
+  }
 }
-
 
 function updateGlobalParametersFromCurrent() {
-    ({ streetCount, avenueCount, streetWidth, blockParcelCount, parcelWidth, parcelDepth, res, parkEdges, parkContinue, needUpdate, squareEdges, broadway } = currentParams);
+  ({
+    streetCount,
+    avenueCount,
+    streetWidth,
+    blockParcelCount,
+    parcelWidth,
+    parcelDepth,
+    res,
+    parkEdges,
+    parkContinue,
+    needUpdate,
+    squareEdges,
+    broadway,
+  } = currentParams);
 }
-
--- coding/grid-erosion/code/events.js
+++ coding/grid-erosion/code/events.js
@@ -1,61 +1,60 @@
 function claimVacantParcel() {
   const vacantParcel = [];
-  for (let parcel of parcels){
-    if(parcel.claimed === false){
+  for (let parcel of parcels) {
+    if (parcel.claimed === false) {
       vacantParcel.push(parcel);
     }
-  } 
+  }
 }
 //spawn a parcel on a random vacant tile belong to the open space, where tile's traffic value is smaller than 2
-function spawnOneParcel(){
+function spawnOneParcel(rate = 0.1) {
   let highTiles = getHighTrafficTiles(round(parcels.length * 0.5));
   console.log(highTiles.length);
-    for (let tile of highTiles) {
-      if (tile.owner == openSpace && random()<0.1) {
-        tile.owner = new SpawnedParcel(tile.i, tile.j);
-        parcels.push(tile.owner);
-        tile.owner.show();
-        tile.owner.tiles.push(tile);
-        tile.streetFront = true;
-        tile.frontageIndex = 0;
-      }
-    }  
+  for (let tile of highTiles) {
+    if (tile.owner == openSpace && random() < rate) {
+      tile.owner = new SpawnedParcel(tile.i, tile.j);
+      parcels.push(tile.owner);
+      tile.owner.show();
+      tile.owner.tiles.push(tile);
+      tile.streetFront = true;
+      tile.frontageIndex = 0;
+    }
+  }
 }
 
-function parcelClaimOneTile() {
+function parcelClaimOneTile(bar = 50) {
   for (let parcel of parcels) {
-    if(parcel.prosperity > random()*50){
+    if (parcel.prosperity > random() * bar) {
       parcel.claimOneTile();
     }
-    
   }
 }
 
-function trafficDecline() {
+function trafficDecline(minus = 1) {
   // reduce tiles' traffic value by 1
   for (let tile of tiles) {
-      if (tile.owner == openSpace) {
-          fill(120)
-          noStroke()
-          rect(tile.i * res, tile.j * res, res, res)
+    if (tile.owner == openSpace) {
+      fill(120);
+      noStroke();
+      rect(tile.i * res, tile.j * res, res, res);
 
-          if (tile.traffic > 0) {
-              tile.traffic = max(tile.traffic -1, 0);
-          }
-          fill(255, 0, 0, tile.traffic * 50)
-          noStroke()
-          rect(tile.i * res, tile.j * res, res, res)
-      }   
+      if (tile.traffic > 0) {
+        tile.traffic = max(tile.traffic - minus, 0);
+      }
+      fill(255, 0, 0, tile.traffic * 50);
+      noStroke();
+      rect(tile.i * res, tile.j * res, res, res);
+    }
   }
 }
 
-function trafficClear(){
+function trafficClear() {
   for (let tile of tiles) {
     tile.traffic = 0;
     if (tile.owner == openSpace) {
-      fill(120)
-      noStroke()
-      rect(tile.i * res, tile.j * res, res, res)
+      fill(120);
+      noStroke();
+      rect(tile.i * res, tile.j * res, res, res);
     }
   }
   routes = [];
@@ -63,86 +62,85 @@ function trafficClear(){
 }
 
 function showTraffic() {
-  const btn = buttonList.find(b => b.html() === "Show Traffic" || b.html() === "Hide Traffic");
-  
+  const btn = buttonList.find(
+    (b) => b.html() === "Show Traffic" || b.html() === "Hide Traffic"
+  );
+
   if (!isTrafficShown) {
-      console.log("Showing traffic");
-      for (let tile of tiles) {
-          if (tile.owner == openSpace) {
-              //background color fill the tile
-              fill(120);
-              noStroke();
-              rect(tile.i * res, tile.j * res, res, res);
+    console.log("Showing traffic");
+    for (let tile of tiles) {
+      if (tile.owner == openSpace) {
+        //background color fill the tile
+        fill(120);
+        noStroke();
+        rect(tile.i * res, tile.j * res, res, res);
 
-              fill(255, 0, 0, tile.traffic * 50);
-              noStroke();
-              rect(tile.i * res, tile.j * res, res, res);
-          }
+        fill(255, 0, 0, tile.traffic * 50);
+        noStroke();
+        rect(tile.i * res, tile.j * res, res, res);
       }
-      redrawPark();
-      isTrafficShown = true;
+    }
+    redrawPark();
+    isTrafficShown = true;
 
-      // change button label to "Hide Traffic"
-      if (btn) btn.html("Hide Traffic");
+    // change button label to "Hide Traffic"
+    if (btn) btn.html("Hide Traffic");
   } else {
-      console.log("Hiding traffic");
-      for (let tile of tiles) {
-          if (tile.owner == openSpace) {
-              tile.show();  // Assuming tile.show() resets the tile's appearance
-          }
+    console.log("Hiding traffic");
+    for (let tile of tiles) {
+      if (tile.owner == openSpace) {
+        tile.show(); // Assuming tile.show() resets the tile's appearance
       }
-      isTrafficShown = false;
+    }
+    isTrafficShown = false;
 
-      // change button label to "Show Traffic"
-      if (btn) btn.html("Show Traffic");
+    // change button label to "Show Traffic"
+    if (btn) btn.html("Show Traffic");
   }
 }
 
-
-function destroyRandomParcels() {
-    console.log("destroy random parcels", parcels.length)
-  const numParcelsToDestroy = Math.floor(parcels.length * 0.2);
+function destroyRandomParcels(rate = 0.2) {
+  console.log("destroy random parcels", parcels.length, "   rate: ", rate);
+  const numParcelsToDestroy = Math.floor(parcels.length * rate);
   for (let i = 0; i < numParcelsToDestroy; i++) {
     const randomIndex = Math.floor(Math.random() * parcels.length);
     parcels.splice(randomIndex, 1);
   }
   console.log(parcels.length);
-  background(120)
-  
-  gridMap = creategridMap(parcels, cellSize)
-  refreshTileOfRemovedParcels()
+  background(120);
+
+  gridMap = creategridMap(parcels, cellSize);
+  refreshTileOfRemovedParcels();
   redrawParcels();
   routes = [];
 }
 
-function destoryParcelsLowProsper(){
+function destoryParcelsLowProsper(rate = 0.1) {
   //sort the parcels by prosperity descending
-  parcels.sort((a, b) => (a.prosperity > b.prosperity) ? 1 : -1)
+  parcels.sort((a, b) => (a.prosperity > b.prosperity ? 1 : -1));
   //remove the parcels with low prosperity
-  for (let i = 0; i < parcels.length * 0.1; i++) {
-      parcels.splice(i, 1);
+  for (let i = 0; i < parcels.length * rate; i++) {
+    parcels.splice(i, 1);
   }
-  background(120)
+  background(120);
 
-  redrawParcels()
-  gridMap = creategridMap(parcels, cellSize)
-  refreshTileOfRemovedParcels()
-
+  redrawParcels();
+  gridMap = creategridMap(parcels, cellSize);
+  refreshTileOfRemovedParcels();
 }
 //all parcels' proserity value reduced by 50%
-function destoryParcelsHalfProsper(){
+function destoryParcelsHalfProsper(rate = 0.5) {
   for (let i = 0; i < parcels.length; i++) {
-      parcels[i].prosperity = parcels[i].prosperity * 0.5;
+    parcels[i].prosperity = parcels[i].prosperity * rate;
   }
-  background(120)
-  redrawParcels()
-  refreshTileOfRemovedParcels()
+  background(120);
+  redrawParcels();
+  refreshTileOfRemovedParcels();
 }
 
-function refreshTileOfRemovedParcels(){
-  for(const tile of tiles){
-    
-    if(!parcels.includes(tile.owner)){
+function refreshTileOfRemovedParcels() {
+  for (const tile of tiles) {
+    if (!parcels.includes(tile.owner)) {
       tile.streetFront = false;
       tile.accessPoint = false;
       tile.owner = openSpace;
@@ -151,7 +149,7 @@ function refreshTileOfRemovedParcels(){
       tile.frontageIndex = 0;
       tile.color = color(120);
     }
-    if (tile.park ==true){
+    if (tile.park == true) {
       tile.owner = park;
       tile.streetFront = false;
       tile.accessPoint = false;
@@ -160,5 +158,52 @@ function refreshTileOfRemovedParcels(){
       tile.frontageIndex = 0;
       tile.color = color(120, 255, 180);
     }
+  }
+}
+
+async function autoGeneration() {
+  const totalIterations = 10;
+  for (let i = 0; i < totalIterations; i++) {
+    // Calculate the progress percentage
+    const progress = (i / totalIterations) * 100;
+
+    // Update the progress bar
+    updateProgressBar(progress);
+
+    // Execute your operations
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        generateRandomTraffic(10);
+        destoryParcelsLowProsper(0.01);
+        // trafficDecline(0.1);
+        destroyRandomParcels(0.02);
+        spawnOneParcel(0.01);
+        generateRandomTraffic(5);
+        parcelClaimOneTile(5);
+        isTrafficShown = false;
+        showTraffic();
+        resolve();
+      }, 0); // Small delay to allow UI updates
+    });
+  }
+  // Update the progress bar to indicate completion
+  updateProgressBar(100); // Set progress to 100% on completion
+}
+
+function updateProgressBar(progress) {
+  const maxProgressBarWidth = 300; // Maximum width of the progress bar at 100%
+  const progressBarElement = document.getElementById("progressBar");
+  if (progressBarElement) {
+    let adjustedProgress = Math.min(progress, 100); // Ensure progress does not exceed 100%
+    let pixelWidth = (adjustedProgress / 100) * maxProgressBarWidth; // Calculate width in pixels
+
+    progressBarElement.style.width = `${pixelWidth}px`;
+    progressBarElement.style.backgroundColor =
+      adjustedProgress < 100 ? "#add8e6" : "#90ee90"; // Lighter colors
+    progressBarElement.style.color = "black"; // Text color
+    progressBarElement.style.textAlign = "center"; // Center the text
+    progressBarElement.style.lineHeight = progressBarElement.style.height; // Align text vertically
+    progressBarElement.style.fontSize = "14px"; // Font size of the text
+    progressBarElement.textContent = `${adjustedProgress.toFixed(0)}%`; // Set the text to show the percentage
   }
 }
